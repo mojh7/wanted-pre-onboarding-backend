203ad43cec5b17865ccfe9aa3d9f13151e7466ab
mojh7 <mjh79017901@gmail.com>
2023-10-06 05:53:56 +0900

    feat: Member entity 추가

---
+++ src/main/java/com/wanted/preonboarding/member/entity/Member.java
@@ -0,0 +1,31 @@
+package com.wanted.preonboarding.member.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Builder
+    public Member(String name) {
+        this.name = name;
+    }
+
+}
