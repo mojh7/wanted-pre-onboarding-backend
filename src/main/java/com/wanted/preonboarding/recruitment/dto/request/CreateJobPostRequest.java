699c66c02a2315414908cb5e0da5b074e62b570d
mojh7 <mjh79017901@gmail.com>
2023-10-06 23:06:40 +0900

    feat: 채용 공고 등록 dto 클래스 추가

---
+++ src/main/java/com/wanted/preonboarding/recruitment/dto/request/CreateJobPostRequest.java
@@ -0,0 +1,53 @@
+package com.wanted.preonboarding.recruitment.dto.request;
+
+import com.wanted.preonboarding.company.entity.Company;
+import com.wanted.preonboarding.recruitment.entity.JobPost;
+import lombok.Getter;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+public class CreateJobPostRequest {
+
+    @Min(0)
+    @Max(Long.MAX_VALUE)
+    private Long companyId;
+
+    @NotBlank
+    @Size(max = 64)
+    private String position;
+
+    @Min(0)
+    @Max(Long.MAX_VALUE)
+    private Long reward;
+
+    @NotBlank
+    @Size(max = 255)
+    private String skills;
+
+    @NotBlank
+    @Size(max = 2000)
+    private String description;
+
+    public CreateJobPostRequest(Long companyId, String position, Long reward,
+                                String skills, String description) {
+        this.companyId = companyId;
+        this.position = position;
+        this.reward = reward;
+        this.skills = skills;
+        this.description = description;
+    }
+
+    public JobPost toEntity(final Company company) {
+        return JobPost.builder()
+                      .company(company)
+                      .position(position)
+                      .reward(reward)
+                      .skills(skills)
+                      .description(description)
+                      .build();
+    }
+}
