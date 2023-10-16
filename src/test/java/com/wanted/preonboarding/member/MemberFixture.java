fc35cdc4944fc53ecfbd7ec5e73c6ef1bfd2b51a
mojh7 <mjh79017901@gmail.com>
2023-10-17 06:29:46 +0900

    test: MemberFixture 생성

---
+++ src/test/java/com/wanted/preonboarding/member/MemberFixture.java
@@ -0,0 +1,29 @@
+package com.wanted.preonboarding.member;
+
+import com.wanted.preonboarding.member.entity.Member;
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class MemberFixture {
+
+    private static Member MEMBER_CHULSOO = Member.builder()
+                                                 .name("김철수")
+                                                 .build();
+
+    private static Member MEMBER_GILDONG = Member.builder()
+                                                 .name("홍길동")
+                                                 .build();
+
+    public static Member MEMBER_CHULSOO() {
+        if (MEMBER_CHULSOO.getId() == null) {
+            ReflectionTestUtils.setField(MEMBER_CHULSOO, "id", 1L);
+        }
+        return MEMBER_CHULSOO;
+    }
+
+    public static Member MEMBER_GILDONG() {
+        if (MEMBER_GILDONG.getId() == null) {
+            ReflectionTestUtils.setField(MEMBER_GILDONG, "id", 2L);
+        }
+        return MEMBER_CHULSOO;
+    }
+}
