4bb9caabd14febf2000742d249937b26154b6a1e
mojh7 <mjh79017901@gmail.com>
2023-10-19 07:04:24 +0900

    feat: dto vaild annotation 수정
     - ApplyJobCreateRequest, JobPostCreateRequest, jobPostUpdateRequest dto 수정

--- src/main/java/com/wanted/preonboarding/applyjob/dto/request/ApplyJobCreateRequest.java
+++ src/main/java/com/wanted/preonboarding/applyjob/dto/request/ApplyJobCreateRequest.java
@@ -4,15 +4,20 @@ import lombok.Getter;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Getter
 public class ApplyJobCreateRequest {
-    @Min(0)
-    @Max(Long.MAX_VALUE)
-    private Long jobPostId;
 
     @Min(0)
     @Max(Long.MAX_VALUE)
+    @NotNull
+    private Long jobPostId;
+
+
+    @Min(0)
+    @Max(Long.MAX_VALUE)
+    @NotNull
     private Long memberId;
 
     public ApplyJobCreateRequest(Long jobPostId, Long memberId) {
