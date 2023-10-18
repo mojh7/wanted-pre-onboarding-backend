09790fe5966e57236415f03d88057a2abf7323d5
mojh7 <mjh79017901@gmail.com>
2023-10-19 07:16:13 +0900

    feat: 일부 dto 클래스 유효성 검증 annotation 수정 (#23)
    * feat: dto vaild annotation 수정
    
     - ApplyJobCreateRequest, JobPostCreateRequest, jobPostUpdateRequest dto 수정
    
    * fix: Api 응답 시 예외 메시지 출력 방식 수정
    
    - 기존에 정의된 errorCode의 메시지 외에 추가 메시지(detail) 입력시 추가 메시지 제대로 출력 안된 것 수정
    
    * chore: application.yml 수정
    
     - 유효성 검증 관련 default 메시지 영문으로 나오도록 수정

--- src/main/java/com/wanted/preonboarding/common/exception/ApplicationException.java
+++ src/main/java/com/wanted/preonboarding/common/exception/ApplicationException.java
@@ -8,28 +8,33 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApplicationException extends RuntimeException {
     private ErrorCode errorCode;
-    private String detail;
+    private String errorMessage;
 
     public ApplicationException(ErrorCode errorCode) {
         super(errorCode.getMessage());
+        errorMessage = errorCode.getMessage();
         this.errorCode = errorCode;
     }
 
     public ApplicationException(ErrorCode errorCode, String detail) {
         super(message(errorCode, detail));
+        errorMessage = message(errorCode, detail);
         this.errorCode = errorCode;
     }
 
     public ApplicationException(Throwable cause) {
         super(cause);
+        errorMessage = errorCode.getMessage();
     }
 
     public ApplicationException(ErrorCode errorCode, Throwable cause) {
         super(errorCode.getMessage(), cause);
+        errorMessage = errorCode.getMessage();
     }
 
     public ApplicationException(ErrorCode errorCode, Throwable cause, String detail) {
         super(message(errorCode, detail), cause);
+        errorMessage = message(errorCode, detail);
     }
 
     private static String message(ErrorCode errorCode, String detail) {
@@ -37,6 +42,6 @@ public class ApplicationException extends RuntimeException {
             return errorCode.getMessage();
         }
 
-        return "${errorCode.getMessage()} ${detail}";
+        return errorCode.getMessage() + detail;
     }
 }
